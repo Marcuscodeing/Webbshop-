dd170f7e50cbccb4dbc90c4c5c2887cc93aeb0b8
Marcus Linder <marcuslinder@Marcuss-MacBook-Air.local>
2024-12-26 23:58:00 +0100

    Fetch klar men bilderna syns inte.

--- webbshop/src/main.ts
+++ webbshop/src/main.ts
@@ -27,19 +27,47 @@ async function getCloths (): Promise<Product[]> {
 }
 
 async function clothProducts () {
-  const products = document.getElementById("product-container");
-  if (!products) {
+  const productsContainer = document.getElementById("product-container");
+  if (!productsContainer) {
     console.error ("Couldent find product");
     return;
   }
 
   const products = await getCloths();
 
-} 
+products.forEach ((product) => {
+  
+  const productArea = document.createElement("div");
+
+  const img = document.createElement("img");
+  img.src = product.image;
+  img.alt = product.title;
+
+  const title = document.createElement("h3");
+  title.textContent = product.title;
+  
+  const description = document.createElement("p");
+  description.textContent = product.description;
+  
+  const price = document.createElement("p");
+  price.textContent = "$" + product.price;
+
+  const button = document.createElement("button");
+  button.textContent = "Add to cart";
+  
+  productArea.appendChild(img);
+  productArea.appendChild(title);
+  productArea.appendChild(description);
+  productArea.appendChild(price);
+  productArea.appendChild(button);
+
+  productsContainer.appendChild(productArea);
+
+});
+}
 
 
-
-
+clothProducts ();
 
 
 
