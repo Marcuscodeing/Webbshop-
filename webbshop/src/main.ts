6792dae8989a8dcdebe0f41a1cb22911a8faccea
Marcus Linder <marcuslinder@Marcuss-MacBook-Air.local>
2024-12-26 23:02:29 +0100

    Skapat interface och fetch function. Inte riktigt klar

--- webbshop/src/main.ts
+++ webbshop/src/main.ts
@@ -1,24 +1,46 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+interface Product {
+  id: number;
+  title: string 
+  price: number;
+  description: string;
+  image: string 
+  category: string;
+}
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+
+async function getCloths (): Promise<Product[]> {
+
+  try {
+    const response = await fetch ("https://api.escuelajs.co/api/v1/categories/1/products");
+    if (!response.ok) {
+      throw new Error ("Not ok");
+    }
+    const clothsData: Product[] = await response.json();
+    console.log(clothsData);
+    return clothsData;
+  } catch (error){
+    console.error ("Failed to fetch cloths:", error);
+    return[];
+  }
+}
+
+async function clothProducts () {
+  const products = document.getElementById("product-container");
+  if (!products) {
+    console.error ("Couldent find product");
+    return;
+  }
+
+  const products = await getCloths();
+
+} 
+
+
+
+
+
+
+
+
