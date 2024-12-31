7fabcae35ba391d72d4f3c0e6f5cf1365094ba82
Marcus Linder <marcuslinder@Marcuss-MacBook-Air.local>
2024-12-31 11:24:26 +0100

    feature brands ser bra ut

--- webbshop/src/main.ts
+++ webbshop/src/main.ts
@@ -2,18 +2,39 @@ import './style.css'
 
 
 const brandPictures = [
-    "./Img/Brand1.jpg",  
+    "./Img/Brand 4.png",  
     "./Img/Brand 2.jpg", 
     "./Img/Brand 3.jpg", 
-    "./Img/Brand 4.png", 
+    "./Img/Brand1.jpg", 
+    
 ];
 
-const container = document.getElementById("app")
+
+const container = document.createElement("div")
+const headline = document.createElement ("h2")
+headline.innerHTML = "Utvalda kollektioner"
+headline.className = "h2-brands"
+
 
 if (container) {
 for (let i = 0; i < brandPictures.length; i++) {
     const images = document.createElement("img");
     images.src = brandPictures[i];
-    container.appendChild(images); 
-}
-}
+    images.className = "brand-images"; 
+
+    const imgText = document.createElement("p");
+    imgText.innerHTML = "Utforska"
+
+    const imageContainer = document.createElement("div");
+    imageContainer.className = "image-container"
+    
+
+    imageContainer.appendChild(images);
+    imageContainer.appendChild(imgText);
+    container.appendChild(imageContainer);
+    
+}};
+
+document.body.appendChild(headline);
+document.body.appendChild(container);
+container.className = "brand-container"
