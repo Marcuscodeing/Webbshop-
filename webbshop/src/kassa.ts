ab9fae2eac82db9ab75f0a9f5c885748bc4fbfb3
Marcus Linder <marcuslinder@Marcuss-MacBook-Air.local>
2025-01-08 21:21:06 +0100

    HEEREGUD så nära nu.

--- webbshop/src/kassa.ts
+++ webbshop/src/kassa.ts
@@ -9,6 +9,8 @@ interface Product {
 
 interface CartItem {
     product: Product;
+    size: string;
+    quantity: number;
 }
 
 // Hämta våra kära produkter 
@@ -31,19 +33,33 @@ let totalWithTax: number = 0;
 let totalWithBoth: number = 0;
 
 
+// Skapa div och loop igenom bild title pris 
+
 const productskassa = document.createElement("div");
 productskassa.className = "products"; 
 
-cartItems.forEach((cartItem) => {
+cartItems.forEach((cartItem, index ) => {
+  const size = cartItem.size;
+  let quantity = cartItem.quantity;
+
 
   const kassaDiv = document.createElement ("div");
   kassaDiv.className = "kassasida"
+
+  const sizeElement = document.createElement("p");
+  sizeElement.textContent = `Size: ${size}`;
+  kassaDiv.appendChild(sizeElement);
+
+
+  const quantityAntal = document.createElement("p");
+  quantityAntal.textContent = `Quantity: ${quantity}`;
+  kassaDiv.appendChild(quantityAntal);
+
 
   const kassaImg = document.createElement("img");
   kassaImg.src = cartItem.product.image;
   kassaImg.alt = cartItem.product.title;
   kassaImg.className = "kassaimg"
-
   kassaDiv?.appendChild(kassaImg);
 
   const kassaTitle = document.createElement("h3");
@@ -52,12 +68,42 @@ cartItems.forEach((cartItem) => {
 
   const kassaPrice = document.createElement("p");
   kassaPrice.innerHTML = `Pris: ${cartItem.product.price}`;
-
-  totalExTax += cartItem.product.price;
-  totalWithTax += cartItem.product.price * momssats;
+  
+  
 
   kassaDiv.appendChild(kassaPrice);
   checkout?.appendChild(kassaDiv);
+
+  const increaseBtn = document.createElement("button");
+  increaseBtn.innerHTML = "+";
+  increaseBtn.addEventListener("click", () => {
+      quantity += 1;
+      cartItems[index].quantity = quantity;
+      localStorage.setItem("cart", JSON.stringify(cartItems));
+      quantityAntal.textContent = `Quantity: ${quantity}`;
+      kassaPrice.textContent = `Price: $${(cartItem.product.price * quantity).toFixed(2)}`;
+  
+      updateSummery ();
+  
+    });
+
+  kassaDiv.appendChild(increaseBtn);
+
+  const decreaseBtn = document.createElement("button");
+  decreaseBtn.innerHTML = "-";
+  decreaseBtn.addEventListener("click", () => {
+      quantity -= 1;
+      cartItems[index].quantity = quantity;
+      localStorage.setItem("cart", JSON.stringify(cartItems));
+      quantityAntal.textContent = `Quantity: ${quantity}`;
+      kassaPrice.textContent = `Price: $${(cartItem.product.price * quantity).toFixed(2)}`;
+  
+      updateSummery();
+  
+    });
+
+  kassaDiv.appendChild(decreaseBtn);
+
 });
 
 checkout?.appendChild(productskassa);
@@ -67,8 +113,24 @@ checkout?.appendChild(productskassa);
 
 const summery = document.createElement("div");
 summery.className = "summering";
+checkout?.appendChild(summery);
+
+const updateSummery = () => {
+
+  totalExTax = 0;
+  totalWithTax = 0;
+
+  cartItems.forEach(cartItem => {
+    totalExTax += cartItem.product.price * cartItem.quantity
+  totalWithTax += cartItem.product.price * cartItem.quantity * momssats;
+
+  });
+
 
 totalWithBoth = totalExTax + totalWithTax;
+
+summery.innerHTML = "";
+
 
 const noTax = document.createElement("p");
 noTax.innerHTML = `Totalt pris (utan moms): <span> ${totalExTax.toFixed(2)}</span>`; 
@@ -88,7 +150,7 @@ const totalSum = document.createElement ("p");
 totalSum.innerHTML = `Totalt pris (inkl moms) <span> ${totalWithBoth.toFixed(2)} </span>`;
 summery.appendChild(totalSum);
 
-checkout?.appendChild(summery);
+};
 
 
 
@@ -98,10 +160,14 @@ totalPaymentText.className = "totalPayment-text";
 totalPaymentText.innerHTML = `Totaltbelopp <span> ${totalWithBoth.toFixed(2)} </span>`;
 totalPayment?.appendChild(totalPaymentText);
 
+};
+
+
 
 const finalButt = document.getElementById("complete-order");
 finalButt?.addEventListener ("click", () => {
   window.location.href = "sista-sidan.html";
 });
 
-};
+
+
