407b2982fefc8d27a95e8b45faeb8d63caf64017
Madiha <madihaiftikhar2013@gmail.com>
2025-01-08 19:43:33 +0100

    b

---
+++ webbshop/src/contact.ts
@@ -0,0 +1,21 @@
+
+
+const myForm = document.getElementById("contact-form") as HTMLFormElement;
+const msg = document.getElementById("message") as HTMLTextAreaElement;
+const emailInput = document.getElementById("e-mail") as HTMLInputElement;
+const telephone = document.getElementById("tel") as HTMLInputElement;
+
+function handleSubmit(event:Event): void{
+    event.preventDefault();
+    const message = msg.value;
+    const email = emailInput.value;
+    const tel = telephone.value;
+    if(!message || !email  || !tel){
+        alert("fields must be filled");
+        return;
+    }
+alert ("form submitted");
+myForm.reset();
+    
+}
+myForm.addEventListener("submit", handleSubmit);
